0a465af529203a62dd9074a0ad89a807cd9b5074
Alex Williams and manasa vesala <alex+manasa@>
2018-10-15 15:11:09 -0700

    Added Bootstrap Panels, and a new button to run the script on the page instead of running on page launch

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
-var str = prompt("Enter sentence to be ciphered");
-// Other way to gather the last character in the sentence.
-//var newstrFL = function(str){
-  //var first = str.charAt(0).toUpperCase();
-  //var last = str.slice(-1).toUpperCase();
-  //return first + last;
-//};
+function script() {
+  var str = prompt("Enter sentence to be ciphered");
+  
+  var newstrFL = function(str){
+    var first = str.charAt(0).toUpperCase();
+    var last = str.slice(-1).toUpperCase();
+    return first + last;
+  };
 
-var newstrLF = function(str){
-  var first = str.charAt(0).toUpperCase();
-  var last = str.charAt(str.length-1).toUpperCase();
-  return last + first;
-};
+  var newstrLF = function(str){
+    var first = str.charAt(0).toUpperCase();
+    var last = str.charAt(str.length-1).toUpperCase();
+    return last + first;
+  };
 
-var count = function(str){
-  var length = (str.length)/2;
-  var letter = str.charAt(length - 1);
-  return letter;
-};
+  var count = function(str){
+    var length = (str.length)/2;
+    var letter = str.charAt(length - 1);
+    return letter;
+  };
 
-var newstrTwo = count(str) + str + newstrLF(str);
+  var newstrTwo = count(str) + str + newstrLF(str);
 
-newstrTwo = newstrTwo.split("");
-newstrTwo = newstrTwo.reverse();
-newstrTwo = newstrTwo.join("");
-alert(newstrTwo);
+  newstrTwo = newstrTwo.split("");
+  newstrTwo = newstrTwo.reverse();
+  newstrTwo = newstrTwo.join("");
+  alert(newstrTwo);
+}
+
+jQuery("p").click(function() {
+  alert("This is a Paragraph.");
+});
